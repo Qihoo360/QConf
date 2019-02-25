c8841aa8644e2d151ff1df794a4f30201719ecc9
wonderful60 <mail@logtous.com>
2019-02-25 14:49:06 +0800

    include_dirs path error

--- driver/python/setup.py
+++ driver/python/setup.py
@@ -1,5 +1,5 @@
 from distutils.core import setup, Extension
 setup(name = 'qconf_py', version = '1.2.2', ext_modules = [Extension('qconf_py', ['lib/python_qconf.cc'],
-     include_dirs=['/usr/local/include/qconf'],
+     include_dirs=['/usr/local/qconf/include'],
      extra_objects=['/usr/local/qconf/lib/libqconf.a']
      )])
